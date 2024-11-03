b3d8a807e5907c8ae2b30501874072bfac4fd124
葉宣余 <hsuanyu@myMac.local>
2024-11-02 20:51:47 -0400

    add CNN

---
+++ ConvNets.py
@@ -0,0 +1,208 @@
+import torch
+import requests
+import random
+from pathlib import Path
+from torch import nn
+from tqdm.auto import tqdm
+from torchvision import datasets, transforms
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+from torchmetrics import ConfusionMatrix
+from mlxtend.plotting import plot_confusion_matrix
+
+# data prep
+train_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True, 
+    transform=transforms.ToTensor(),
+    target_transform=None
+)
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=transforms.ToTensor(),
+    target_transform=None
+)
+train_dataloader = DataLoader(dataset=train_data,
+                              batch_size=32,
+                              shuffle=True)
+test_dataloader = DataLoader(dataset=test_data,
+                             batch_size=32,
+                             shuffle=False)
+if Path("helper_functions.py").is_file():
+    print("helper_functions.py already exists")
+else:
+    print("Downloading helper_functions.py")
+    requests = requests.get("https://raw.githubusercontent.com/mrdbourke/pytorch-deep-learning/main/helper_functions.py")
+    with open("helper_functions.py", "wb") as f:
+        f.write(requests.content)
+
+# create CNN model
+class FashionMNISTModelV1(nn.Module):
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
+        super().__init__()
+        self.block_1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=input_shape,
+                out_channels=hidden_units,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels=hidden_units,
+                out_channels=hidden_units,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(
+                kernel_size=2
+            ))
+        self.block_2 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=hidden_units,
+                out_channels=hidden_units,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels=hidden_units,
+                out_channels=hidden_units,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(
+                kernel_size=2
+            ))
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=hidden_units*7*7,
+                      out_features=output_shape)
+        )
+    def forward(self, x):
+        x = self.block_1(x)
+        x = self.block_2(x)
+        x = self.classifier(x)
+        return x
+
+def train_step(model: torch.nn.Module,
+               data_loader: DataLoader,
+               loss_fn: torch.nn.Module,
+               optimizer: torch.optim.Optimizer,
+               accuracy_fn,
+               device: torch.device):
+    train_loss, train_acc = 0, 0
+    model.to(device)
+    for batch, (X, y) in enumerate(data_loader):
+        X, y = X.to(device), y.to(device)
+        train_pred = model(X)
+        loss = loss_fn(train_pred, y.type(torch.long))
+        train_loss += loss
+        train_acc += accuracy_fn(y, train_pred.argmax(dim=1))
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        if batch%400 == 0:
+            print(f"Looked at {batch*len(X)}/{len(data_loader.dataset)} samples.")
+    train_loss /= len(data_loader)
+    train_acc /= len(data_loader)
+    print(f"Train loss: {train_loss:.4f}, Train Acc: {train_acc:.4f}")
+
+
+def test_step(model: torch.nn.Module,
+              data_loader: DataLoader,
+              loss_fn: torch.nn.Module,
+              accuracy_fn,
+              device: torch.device):
+    test_loss, test_acc = 0, 0
+    model.to(device)
+    model.eval()
+    with torch.inference_mode():
+        for X_test, y_test in data_loader:
+            X_test, y_test = X_test.to(device), y_test.to(device)
+            test_pred = model(X_test)
+            test_loss += loss_fn(test_pred, y_test.type(torch.long))
+            test_acc += accuracy_fn(y_test, test_pred.argmax(dim=1))
+        test_loss /= len(data_loader)
+        test_acc /= len(data_loader)
+    print(f"Test loss: {test_loss:.4f}, Test Acc: {test_acc:.4f}")
+
+
+
+# train the model
+from helper_functions import accuracy_fn
+epochs =20
+class_names = train_data.classes
+model = FashionMNISTModelV1(input_shape=1, hidden_units=10, output_shape=len(class_names))
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(params=model.parameters(), lr=0.01)
+device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+for epoch in tqdm(range(epochs)):
+    print(f"Epoch: {epoch} \n-----")
+    train_step(model=model,
+               data_loader=train_dataloader,
+               loss_fn=loss_fn,
+               optimizer=optimizer, 
+               accuracy_fn=accuracy_fn,
+               device=device)
+    test_step(model=model,
+              data_loader=test_dataloader,
+              loss_fn=loss_fn,
+              accuracy_fn=accuracy_fn,
+              device=device)
+    
+
+# Inspect result
+test_samples = []
+test_labels = []
+l = random.sample(list(test_data), 9)
+for sample, label in l:
+    test_samples.append(sample)
+    test_labels.append(label)
+
+def make_predictions(model: torch.nn.Module, 
+                     data: list,
+                     device: torch.device):
+    model.to(device)
+    model.eval()
+    pred_probs = []
+    with torch.inference_mode():
+        for sample in data:
+            sample = torch.unsqueeze(sample, dim=0).to(device)
+
+            pred_logit = model(sample)
+            pred_prob = torch.softmax(pred_logit.squeeze(), dim=0)
+            pred_probs.append(pred_prob)
+    return torch.stack(pred_probs)
+
+
+pred_probs = make_predictions(model, test_samples, device)
+pred_classes = pred_probs.argmax(dim=1)
+
+# Inspect result with confusion matrix
+y_preds = []
+model.to(device)
+model.eval()
+with torch.inference_mode():
+    for X, y in tqdm(test_dataloader):
+        X, y=X.to(device), y.to(device)
+        y_logits = model(X)
+        y_pred = torch.softmax(y_logits.squeeze(), dim=0).argmax(dim=1)
+        y_preds.append(y_pred.cpu())
+y_preds = torch.cat(y_preds)
+
+confmat = ConfusionMatrix(task="multiclass", num_classes=len(class_names))
+confmat_tensor = confmat(preds=y_preds,
+                         target=test_data.targets)
+plot_confusion_matrix(conf_mat=confmat_tensor.numpy(), class_names=class_names)
+plt.show()
